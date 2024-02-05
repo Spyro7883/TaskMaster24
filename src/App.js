e1441d73298d55e93020b7d5a5ba6011ea827c6d
Spyro7883 <spinu11_2003@yahoo.ro>
2024-02-05 18:06:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+// import { createTask, copyTask, removeTask, createList, copyList, removeList } from "./TodoFunctions.js";
+import { useState } from 'react'
 
-function App() {
+
+export default function App() {
+  const [state, setState] = useState('')
+  const [tasksOptions, setTasksOptions] = useState('')
+  const [containerOptions, setContainerOptions] = useState('')
+  const [containerList, setContainerList] = useState(() => {
+    const savedContainers = localStorage.getItem('containerList');
+    return savedContainers ? JSON.parse(savedContainers) : [];
+  });
+  const [tasksList, setTasksList] = useState(() => {
+    const savedTasks = localStorage.getItem('tasksList');
+    return savedTasks ? JSON.parse(savedTasks) : [];
+
+  });
+  const createContainer = () => {
+    console.log("in creation")
+    const entityContainer = []
+    setContainerList([...containerList, entityContainer])
+    // containerList.map((values) => console.log(values))
+  }
+
+  const createTask = () => {
+    console.log("in creation")
+    const entityTask = []
+    setTasksList([...tasksList, entityTask])
+  }
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setState(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <header>
+        <h1>
+          TodoList
+        </h1>
 
-export default App;
+      </header>
+      <main>
+        {/* {containerList.map((container) =>
+          <section>
+            {container.title ? <input>{container.title}</input> : <input>Enter list title</input>}
+            <button onClick={() => { setContainerOptions(true) }}>
+              <div>
+                <button onClick={setContainerOptions(false)}>❌</button>
+                 <button onClick={'copyContainer'}>Copy List</button>
+                  <button onClick={'removeContainer'}>Remove List</button>
+              </div>
+            </button>
+
+            {tasksList.map((task) =>
+              <div>
+                <p>{task.name}</p>
+                <button onClick={() => { setTasksOptions(true) }}>
+                  <div>
+                    <button onClick={setTasksOptions(false)}>Option</button>
+                    <button onClick={'copyInput'}><Img /></button>
+                    <button onClick={'removeInput'}><Img /></button>
+                  </div>
+                </button>
+              </div>
+            )}
+            <button onClick={'createInput'}>Add new task</button>
+          </section>)
+        } */}
+        <button onClick={createContainer} >Add a new list</button>
+        {containerList.map((item, index) =>
+          <div key={index}>
+            {tasksList.map((item, index) =>
+              <input key={index} onClick={handleInputChange}>
+
+              </input>)}
+            <button onClick={createTask} >Add a new task</button>
+          </div>
+        )}
+      </main>
+      <footer />
+    </>
+  );
+
+}
