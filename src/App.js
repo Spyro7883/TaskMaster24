e5232e237b3d8a5dab52b322750cd0e1335fb8ae
Spyro7883 <spinu11_2003@yahoo.ro>
2024-02-11 22:39:38 +0200

    remove, showOptions and localStorage functionality added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import './App.css';
-// import { createTask, copyTask, removeTask, createList, copyList, removeList } from "./TodoFunctions.js";
 import { useState } from 'react'
 
-
 export default function App() {
-  const [tasksOptions, setTasksOptions] = useState('')
-  const [containerOptions, setContainerOptions] = useState('')
   const [containerList, setContainerList] = useState(() => {
     const savedContainers = localStorage.getItem('containerList');
     return savedContainers ? JSON.parse(savedContainers) : [];
   });
+  const [manageOptions, setManageOptions] = useState(new Map())
 
   const createEntity = (index) => {
     let newContainerList = [...containerList];
+    let key;
     if (index === undefined) {
-      newContainerList.push(["Add a title"])
-    } else if (newContainerList[index]) {
+      newContainerList.push(["Add a title"]);
+      key = `${newContainerList.length - 1}`;
+      manageOptions.set(key, false);
+    } else if (newContainerList[index] && newContainerList[index][newContainerList[index].length - 1] !== "") {
       newContainerList[index].push("");
-    } else {
+      key = `${newContainerList.length - 1}-${index}`;
+      manageOptions.set(key, false);
+    } else if (newContainerList[index][newContainerList[index].length - 1] === "") {
+      console.log("Last task is empty");
+    }
+    else {
       console.log("Container does not exist");
     }
     setContainerList(newContainerList)
+    setManageOptions(manageOptions)
+    localStorage.setItem('containerList', JSON.stringify(newContainerList));
   }
 
   const copyEntity = (containerIndex, taskIndex) => {
@@ -30,15 +37,23 @@ export default function App() {
     } else if (newContainerList[containerIndex][taskIndex]) {
       newContainerList[containerIndex].splice(taskIndex + 1, 0, newContainerList[containerIndex][taskIndex])
     } else {
-      console.log("Container can't be copied");
+      console.log("Task can't be copied");
     }
     setContainerList(newContainerList)
+    localStorage.setItem('containerList', JSON.stringify(newContainerList));
   }
 
-  const removeContainer = (containerIndex) => {
-    // let newContainerList = [...containerList];
-    // newContainerList.splice(containerIndex + 2, 1, newContainerList[containerIndex].filter(task => task));
-    // setContainerList(newContainerList)
+  const removeEntity = (containerIndex, taskIndex) => {
+    let newContainerList = [...containerList];
+    if (taskIndex === undefined) {
+      newContainerList.splice(containerIndex, 1);
+    } else if (newContainerList[containerIndex][taskIndex]) {
+      newContainerList[containerIndex].splice(taskIndex, 1);
+    } else {
+      console.log("Task can't be removed");
+    }
+    setContainerList(newContainerList)
+    localStorage.setItem('containerList', JSON.stringify(newContainerList));
   }
 
   const handleInputChange = (containerIndex, taskIndex) => (e) => {
@@ -54,6 +69,20 @@ export default function App() {
       console.log("Container does not exist");
     }
     setContainerList(newContainerList);
+    localStorage.setItem('containerList', JSON.stringify(newContainerList));
+  }
+
+  const showOptions = (state, containerIndex, taskIndex) => {
+    let newOptions = new Map(manageOptions);
+
+    if (taskIndex === undefined) {
+      newOptions.set(containerIndex, !state)
+    } else if (taskIndex !== undefined) {
+      newOptions.set(`${containerIndex}-${taskIndex}`, !state)
+    } else {
+      console.log("Options can't be shown");
+    }
+    setManageOptions(newOptions)
   }
 
   return (
@@ -62,42 +91,25 @@ export default function App() {
         <h1>
           TodoList
         </h1>
-
       </header>
       <main>
-        {/* {containerList.map((container) =>
-          <section>
-            <button onClick={() => { setContainerOptions(true) }}>
-              <div>
-                <button onClick={setContainerOptions(false)}>❌</button>
-                 <button onClick={'copyContainer'}>Copy List</button>
-                  <button onClick={'removeContainer'}>Remove List</button>
-              </div>
-            </button>
-
-            {tasksList.map((task) =>
-              <div>
-                <p>{task.name}</p>
-                <button onClick={() => { setTasksOptions(true) }}>
-                  <div>
-                    <button onClick={setTasksOptions(false)}>Option</button>
-                    <button onClick={'copyInput'}><Img /></button>
-                    <button onClick={'removeInput'}><Img /></button>
-                  </div>
-                </button>
-              </div>
-            )}
-            <button onClick={'createInput'}>Add new task</button>
-          </section>)
-        } */}
         <button onClick={() => createEntity()} >Add a new list</button>
-        <button onClick={() => console.log(containerList)} >Show container list</button>
         {Object.entries(containerList).map(([containerIndex, tasks]) => (
-          <div key={containerIndex}>
-            <button onClick={() => copyEntity(containerIndex)}>Copy container</button>
-            <button onClick={() => removeContainer(containerIndex)}>Remove container</button>
-            {tasks.map((task, taskIndex) => (<div>
-              <button onClick={() => copyEntity(containerIndex, taskIndex)}>Copy task</button>
+          <section key={containerIndex} style={{ display: "flex" }}>
+            <div>
+              <button onClick={() => showOptions(manageOptions.get(containerIndex), containerIndex)}>Container Options</button>
+              {manageOptions.get(containerIndex) ? <>
+                <button onClick={() => copyEntity(containerIndex)}>Copy container</button>
+                <button onClick={() => removeEntity(containerIndex)}>Remove container</button></> : <></>}
+            </div>
+
+            {tasks.map((task, taskIndex) => (<div key={taskIndex}>
+              <div>
+                <button onClick={() => showOptions(manageOptions.get(`${containerIndex}-${taskIndex}`), containerIndex, taskIndex)}>Task Options</button>
+                {manageOptions.get(`${containerIndex}-${taskIndex}`) ? <>
+                  <button onClick={() => copyEntity(containerIndex, taskIndex)}>Copy task</button>
+                  <button onClick={() => removeEntity(containerIndex, taskIndex)}>Remove task</button></> : <></>}
+              </div>
               <input
                 key={`${containerIndex}-${taskIndex}`}
                 type="text"
@@ -107,10 +119,10 @@ export default function App() {
             </div>
             ))}
             <button onClick={() => createEntity(containerIndex)}>Add a new task</button>
-          </div>
+          </section>
         ))}
 
-      </main>
+      </main >
       <footer />
     </>
   );
